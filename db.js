625c33709ec9258e169b33ad8b51d8d520a94afd
ME.AT <meatish2008@gmail.com>
2025-08-07 16:44:45 +0330

    feat: Associate Implementation

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const { Sequelize } = require("sequelize");
 
 const config = require("./config");
+const User = require("./models/User");
+const TagsArticles = require("./models/TagsArticles");
+const Tag = require("./models/Tag");
+const Article = require("./models/Article");
 
 const db = new Sequelize({
   host: config.db.host,
@@ -10,6 +14,28 @@ const db = new Sequelize({
   database: config.db.database,
   dialect: config.db.dialect,
   logging: config.isProduction ? false : console.log,
+});
+
+User.hasMany(Article, {
+  foreignKey: "author_id",
+  onDelete: "CASCADE ",
+});
+
+Article.belongsToMany(User, {
+  foreignKey: "author_id",
+  as: "author",
+});
+
+Article.belongsToMany(Tag, {
+  through: TagsArticles,
+  onDelete: "CASCADE",
+  foreignKey: "article_id",
+});
+
+Tag.belongsToMany(Article, {
+  through: TagsArticles,
+  onDelete: "CASCADE",
+  foreignKey: "tag_id",
 });
 
 module.exports = db;
